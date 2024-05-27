90cf6fbbbd232f08e0dee9d8572b631627785211
Keve <clevertonruppenthal1@gmail.com>
2024-05-27 19:46:30 -0300

    Ajustes container bordas aredondadas, aspect e outros.

--- pages/plant.php
+++ pages/plant.php
@@ -44,9 +44,9 @@ if (isset($_GET['id'])) {
     <div class="container mx-auto p-4">
         <?php
         if ($plant && $plant['name'] !== 'Planta não encontrada') {
-            echo '<div class="bg-white dark:bg-gray-800 p-6 mx-auto overflow-y-auto max-w-full sm:max-w-md">';
+            echo '<div class="bg-white dark:bg-gray-800 p-6 mx-auto overflow-y-auto max-w-full sm:max-w-md rounded-lg">';
             echo '<div class="flex justify-center">';
-            echo '<img src="/'.htmlspecialchars($plant["image_path"]).'" alt="'.htmlspecialchars($plant["name"]).'" class="block w-full aspect-w-10 aspect-h-8 rounded-lg overflow-hidden">';
+            echo '<img src="/'.htmlspecialchars($plant["image_path"]).'" alt="'.htmlspecialchars($plant["name"]).'" class="block w-full aspect-square object-cover rounded-lg overflow-hidden">';
             echo '</div>';
             echo '<h1 class="text-2xl font-bold mt-2 text-center text-gray-900 dark:text-gray-100">'.htmlspecialchars($plant["name"]).'</h1>';
             echo '<p class="text-lg font-bold text-center text-gray-700 dark:text-gray-300 mt-1"><i>'.htmlspecialchars($plant["scientific_name"]).'</i></p>';
