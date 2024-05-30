533f2fe2955a79eddd1aed0185401190442bca8e
Polixter <clevertonruppenthal1@gmail.com>
2024-05-30 09:00:42 -0300

    Alteração estilos pagina planta e navbar

--- components/navbar.php
+++ components/navbar.php
@@ -2,7 +2,7 @@
 include $_SERVER['DOCUMENT_ROOT'].'/utils/db.php';
 ?>
 
-<nav class="bg-gray-50 border-gray-200 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+<nav class="bg-gray-200 border-gray-400 dark:border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
     <div class="container mx-auto flex flex-wrap items-center justify-between">
         <a href="/" class="flex">
             <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="50" height="50">
--- pages/plant.php
+++ pages/plant.php
@@ -39,26 +39,26 @@ if (isset($_GET['id'])) {
     <link href="/style.css" rel="stylesheet" />
 </head>
 
-<body class="bg-gray-200 dark:bg-gray-900 dark:text-white">
+<body class="bg-white dark:bg-gray-900 dark:text-white">
     <?php include $_SERVER['DOCUMENT_ROOT'].'/components/navbar.php'; ?>
     <div class="container mx-auto p-4">
         <?php
         if ($plant && $plant['name'] !== 'Planta não encontrada') {
-            echo '<div class="bg-white dark:bg-gray-800 p-6 mx-auto overflow-y-auto max-w-full sm:max-w-md rounded-lg">';
+            echo '<div class="bg-white dark:bg-gray-900 p-4 mx-auto overflow-y-auto max-w-full sm:max-w-md rounded-lg">';
             echo '<div class="flex justify-center">';
             echo '<img src="/'.htmlspecialchars($plant["image_path"]).'" alt="'.htmlspecialchars($plant["name"]).'" class="block w-full aspect-square object-cover rounded-lg overflow-hidden">';
             echo '</div>';
             echo '<h1 class="text-2xl font-bold mt-2 text-center text-gray-900 dark:text-gray-100">'.htmlspecialchars($plant["name"]).'</h1>';
-            echo '<p class="text-g text-center text-gray-700 dark:text-gray-300 mt-1"><i>'.htmlspecialchars($plant["scientific_name"]).'</i></p>';
-            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-semibold mt-4 text-gray-900 dark:text-gray-100">Sobre</h2>';
-            echo '<p class="mt-2 text-gray-700 dark:text-gray-300">'.nl2br(htmlspecialchars($plant["description"])).'</p>';
-            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-semibold mt-4 text-gray-900 dark:text-gray-100">Plantio</h2>';
-            echo '<p class="mt-1 text-gray-700 dark:text-gray-300">'.nl2br(htmlspecialchars($plant["planting_instructions"])).'</p>';
-            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-semibold mt-4 text-gray-900 dark:text-gray-100">Cuidados</h2>';
-            echo '<p class="mt-1 text-gray-700 dark:text-gray-300">'.nl2br(htmlspecialchars($plant["care_instructions"])).'</p>';
-            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-semibold mt-4 text-gray-900 dark:text-gray-100">Ficha Técnica</h2>';
-            echo '<p class="mt-1 text-gray-700 dark:text-gray-300">'.nl2br(htmlspecialchars($plant["technical_sheet"])).'</p>';
-            echo '<ul class="mt-1 text-gray-700 dark:text-gray-300">';
+            echo '<p class="text-g text-center text-gray-900 dark:text-gray-300 mt-1"><i>'.htmlspecialchars($plant["scientific_name"]).'</i></p>';
+            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-bold mt-4 text-lime-700 dark:text-gray-100">Sobre</h2>';
+            echo '<p class="mt-2 text-gray-900 dark:text-gray-300 font-semibold">'.nl2br(htmlspecialchars($plant["description"])).'</p>';
+            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-bold mt-4 text-lime-700 dark:text-gray-100">Plantio</h2>';
+            echo '<p class="mt-1 text-gray-900 dark:text-gray-300 font-semibold">'.nl2br(htmlspecialchars($plant["planting_instructions"])).'</p>';
+            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-bold mt-4 text-lime-700 dark:text-gray-100">Cuidados</h2>';
+            echo '<p class="mt-1 text-gray-900 dark:text-gray-300 font-semibold">'.nl2br(htmlspecialchars($plant["care_instructions"])).'</p>';
+            echo '<h2 class="border-t border-gray-300 dark:border-gray-500 text-xl font-bold mt-4 text-lime-700 dark:text-gray-100">Ficha Técnica</h2>';
+            echo '<p class="mt-1 text-gray-9 00 dark:text-gray-300 leading-relaxed">'.nl2br(htmlspecialchars($plant["technical_sheet"])).'</p>';
+            echo '<ul class="mt-1 text-gray-900 dark:text-gray-300 font-semibold">';
             echo '<li><strong>Toxicidade:</strong> '.($plant["toxicity"] ? "Sim" : "Não").'</li>';
             echo '<li><strong>Requisitos de Sol:</strong> '.htmlspecialchars($plant["sun_requirements"]).'</li>';
             echo '<li><strong>Tipo:</strong> '.htmlspecialchars($plant["type"]).'</li>';
